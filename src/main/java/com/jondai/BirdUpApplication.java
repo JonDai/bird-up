3ed05a83b0fb499c0d7605a99ad1962400bed6b4
daipengwei <897007549@qq.com>
2018-05-08 21:47:27 +0800

    init

---
+++ src/main/java/com/jondai/BirdUpApplication.java
@@ -0,0 +1,12 @@
+package com.jondai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdUpApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jondai/BirdUpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jondai;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BirdUpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
